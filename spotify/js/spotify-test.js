079dd2a6c6b375aeb4e238be77a2f7f359366682
Wes Modes <wmodes@gmail.com>
2022-10-27 16:16:04 -0700

    Update spotify-test.js

--- spotify/js/spotify-test.js
+++ spotify/js/spotify-test.js
@@ -19,7 +19,7 @@ spotifyApi.setAccessToken(accessToken);
 // get Elvis' albums, using Promises through Promise, Q or when
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
   function (data) {
-    console.log('Artist albums', data);
+    console.log('Artist albums', JSON.stringify(data, null, 2));
   },
   function (err) {
     console.error(err);
