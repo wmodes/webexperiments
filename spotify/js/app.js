95f79e07f4082de3077dcab1462d822669b439fe
Wes Modes <wmodes@gmail.com>
2022-10-27 16:39:15 -0700

    Update app.js

--- spotify/js/app.js
+++ spotify/js/app.js
@@ -19,7 +19,7 @@ spotifyApi.setAccessToken(accessToken);
 // get Elvis' albums, using Promises through Promise, Q or when
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
   function (data) {
-    console.log('Artist albums:', data);
+    // console.log('Artist albums:', data);
     showAlbums(data);
   },
   function (err) {
@@ -30,10 +30,10 @@ spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
 function showAlbums(data) {
   // console.log("array:", data.items);
   albumList = data.items;
-  console.log("albumList:", albumList)
+  // console.log("albumList:", albumList)
   // $("#albums").html("Album data: <pre>", JSON.stringify(albumList, null, '\t'), "</pre>");
   for (i=0;i<albumList.length;i++) {
-    var newAlbum = $("#albums").append("<div>");
+    var newAlbum = $("<div></div>");
     newAlbum.append("<h3>" + albumList[i].name);
   }
 }
