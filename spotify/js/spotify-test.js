80dd32dddcc864f7c8c5b7a02af3973929b7cb34
Wes Modes <wmodes@gmail.com>
2022-10-27 15:49:14 -0700

    Update spotify-test.js

--- spotify/js/spotify-test.js
+++ spotify/js/spotify-test.js
@@ -1,10 +1,28 @@
 
 
+// get parameters
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+var accessToken = getUrlParameter('access_token');
+console.log("Access token:", accessToken);
+
 var spotifyApi = new SpotifyWebApi();
-// spotifyApi.setAccessToken('<here_your_access_token>');
-// spotifyApi.setPromiseImplementation(Q);
-
-
+spotifyApi.setAccessToken('accessToken');
+spotifyApi.setPromiseImplementation(Q);
 
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err, data) {
   if (err) console.error(err);
