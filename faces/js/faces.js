242fbc8082cd97aa51c588656c5252b4359fc73f
Wes Modes <wmodes@gmail.com>
2023-12-05 12:29:04 -0800

    testing

---
+++ faces/js/faces.js
@@ -0,0 +1,25 @@
+/*
+  faces.js - javascript for faces
+
+  author: Wes Modes, Shelby Decker
+  date: 2023
+*/
+
+$(document).ready(function() {
+  run()
+})
+    
+async function run() {
+  // load the models
+  await faceapi.loadMtcnnModel('/')
+  await faceapi.loadFaceRecognitionModel('/')
+  
+  // try to access users webcam and stream the images
+  // to the video element
+  const videoEl = document.getElementById('inputVideo')
+  navigator.getUserMedia(
+    { video: {} },
+    stream => videoEl.srcObject = stream,
+    err => console.error(err)
+  )
+}
